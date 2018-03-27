45feba213944b4b236f4a37bd5f823049a82483d
DenisRupp <denis.rupp@n3wnormal.com>
2018-03-27 08:02:30 -0700

    Change precommit hook

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = require('./config/express');
 const models = require('./config/sequelize');
 
 models.sequelize.sync().then(() => {
+
   // listen to requests
   app.listen(port, () => console.info(`Server started on port ${port} (${env})`));
 });
