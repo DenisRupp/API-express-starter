c74a02f9f81a410e194696a70d079392cc649c21
DenisRupp <denis.rupp@n3wnormal.com>
2018-04-05 07:18:36 -0700

    Add clear db before start tests

--- src/api/tests/integration/helper.js
+++ src/api/tests/integration/helper.js
@@ -2,11 +2,7 @@ const db = require('../../models');
 /**
  * Global mocha hooks
  */
-before(() => {
-  console.log('--> Migrate database');
-  console.log('--> Run seeds database');
-});
-
-after(() => {
-  console.log('--> Delete database');
+before(async () => {
+  await db.sequelize.sync({ force: true });
+  console.log('--> Drop database');
 });
