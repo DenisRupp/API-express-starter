ed8c7a47f31f30e74b77c1be8a258c09a279defa
DenisRupp <denis.rupp@n3wnormal.com>
2018-04-12 04:23:30 -0700

    Add trevice CI

--- src/database/config.example.js
+++ src/database/config.example.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   test: {
     username: 'postgres',
-    password: 'password',
+    password: '',
     database: 'node-boilerplate_test',
     host: '127.0.0.1',
     dialect: 'postgres',
