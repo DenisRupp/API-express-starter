180a507f076c08a3d5e869bd9b90629094d7cff3
Den Rupp <rupp1993@gmail.com>
2018-05-29 18:32:33 +0300

    Change ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,35 +3,35 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
     {
-      name      : 'API',
-      script    : 'src',
-      watch     : false,
-      exec_mode : "cluster_mode",
-      instances : "max",
+      name: 'API',
+      script: 'src',
+      watch: false,
+      exec_mode: 'cluster_mode',
+      instances: 'max',
       ignoreWatch: ['.idea/*', '.git/*'],
-      log_date_format : "DD-MM-YY HH:mm",
+      log_date_format: 'DD-MM-YY HH:mm',
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: 'development',
       },
-      env_staging : {
+      env_staging: {
         NODE_ENV: 'production',
       },
-      env_production : {
+      env_production: {
         NODE_ENV: 'production',
       },
     },
     {
-      name      : 'API_LOCAL',
-      script    : 'src',
-      watch     : true,
-      exec_mode : "fork",
+      name: 'API_LOCAL',
+      script: 'src',
+      watch: true,
+      exec_mode: 'fork',
       ignoreWatch: ['.idea/*', '.git/*'],
-      log_date_format : "DD-MM-YY HH:mm",
+      log_date_format: 'DD-MM-YY HH:mm',
       env: {
-        NODE_ENV: "development",
-        node_args: ["--debug=7000"]
+        NODE_ENV: 'development',
+        node_args: ['--debug=7000'],
       },
     },
   ],
@@ -40,25 +40,17 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    dev: {
+      user: 'ubuntu',
+      host: 'host',
+      key: 'path to pem',
+      ref: 'git@github.com:DenisRupp/API-express-starter.git',
+      path: '/var/www/API-express-starter',
+      ssh_options: 'ForwardAgent=yes',
+      'post-deploy': 'cp ../shared/.env .env && yarn && yarn start',
+      'pre-setup': 'npm install -g yarn && npm install -g pm2 && npm install -g apidoc ',
+      'post-setup': 'yarn && cp src/database/config.example.js src/database/config.js',
     },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
-    }
-  }
+  },
 };
